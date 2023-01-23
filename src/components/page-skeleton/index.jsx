1ab96680530ff2e7813228030384cec3523d0abd
guinnod <210103064@stu.sdu.edu.kz>
2023-01-23 15:25:27 +0600

    footer stays

--- src/components/page-skeleton/index.jsx
+++ src/components/page-skeleton/index.jsx
@@ -7,9 +7,11 @@ import './page-skeleton.css';
 export const PageSkeleton = ({ header, footer, children, timeLeft, countDown }) => {
     return (
         <div className='page-skeleton__anchor'>
-            {header === 'home' ? <HeaderHome /> : header === 'working' ? <HeaderWorking timeLeft={timeLeft} countDown={countDown} /> : <HeaderMain />}
-            <div className='page-skeleton__content'>
-                {children}
+            <div>
+                {header === 'home' ? <HeaderHome /> : header === 'working' ? <HeaderWorking timeLeft={timeLeft} countDown={countDown} /> : <HeaderMain />}
+                <div className='page-skeleton__content'>
+                    {children}
+                </div>
             </div>
             {footer ? <></> : <Footer />}
         </div>
