60731497b372b39ed56d0a526fc009d7c80f4a57
guinnod <210103064@stu.sdu.edu.kz>
2023-01-26 18:14:35 +0600

    scrollbar

--- src/components/cards/card-theory/index.jsx
+++ src/components/cards/card-theory/index.jsx
@@ -1,15 +1,22 @@
+import { useSelector } from 'react-redux';
 import './card-theory.css';
 export const CardTheory = ({ question, answer, flip, setFlip }) => {
-    
-    
+    const language = useSelector(state => state.language.value.theory);
+
     return (
         <div className={flip ? 'card-theory__root' : 'card-theory__root card-theory__anchor--flipped'}
             onClick={() => {
-                setFlip(!flip);
+                if (window.innerWidth > 850) {
+                    setFlip(!flip);
+                }
             }}>
             <div className='card-theory__anchor'>
                 <div className="card-theory__text">
                     {question}
+                </div>
+                <div className='card-theory__button__anchor'>
+                    <div className='card-theory__button theory__count theory__count--red'>{language.dontKnow}</div>
+                    <div className='card-theory__button theory__count'>{language.know}</div>
                 </div>
             </div>
             <div className='card-theory__anchor card-theory__anchor--flipped card-theory__anchor--back'>
--- src/components/universals/universal-theory/index.jsx
+++ src/components/universals/universal-theory/index.jsx
@@ -1,11 +1,12 @@
 import { CardTheory } from "../../cards/card-theory";
 import { PageSkeleton } from '../../page-skeleton';
 import { LazyImage } from '../../lazy-image';
-import { repeat, shuffle } from "../../../media";
+import { next, previous, repeat, shuffle } from "../../../media";
 import { useDispatch, useSelector } from "react-redux";
 import { theoryActions } from "../../../store/theory";
 import { useState } from 'react';
 import './universal-theory.css';
+
 
 export const UniversalTheory = () => {
     const theory = useSelector(state => state.theory);
@@ -17,6 +18,7 @@ export const UniversalTheory = () => {
         dispatch(theoryActions.select(type));
     }
     const [flip, setFlip] = useState(true);
+
     const language = useSelector(state => state.language.value.theory);
     const userSelect = (type) => {
         if (flip) {
@@ -32,6 +34,7 @@ export const UniversalTheory = () => {
         }, 1050);
 
     }
+
     return (
         <PageSkeleton footer={true} header='home'>
             {theory.partEnded ? <div>Part Ended <div onClick={() => {
@@ -41,22 +44,24 @@ export const UniversalTheory = () => {
                 <LazyImage src={repeat} className='theory__regime' />
             </div> :
                 <div className="theory__anchor">
-                    <div onClick={() => { userSelect("DONT_KNOW") }} className="theory__part theory__part--red">
+                    <div onClick={() => { if (window.innerWidth > 850) { userSelect("DONT_KNOW"); } }} className="theory__part theory__part--red">
                         <div className="theory__count theory__count--red  ">{theory.dontKnows}</div>
-                        <div className="theory__button theory__button--red">
+                        <div className="theory__button theory__button--red" onClick={() => { userSelect("DONT_KNOW"); }}>
                             {language.dontKnow}
                         </div>
+                        <LazyImage src={previous} className='theory__button__navigate' onClick={() => { change("PREVIOUS"); }} />
                         <div className="theory__fill">
 
                         </div>
                     </div>
 
                     <CardTheory flip={flip} setFlip={setFlip} question={theory.question} answer={theory.answer} />
-                    <div onClick={() => { userSelect("KNOW") }} className="theory__part">
+                    <div onClick={() => { if (window.innerWidth > 850) { userSelect("KNOW"); } }} className="theory__part">
                         <div className="theory__count">{theory.knows}</div>
-                        <div className="theory__button">
+                        <div className="theory__button" onClick={() => { userSelect("KNOW"); }}>
                             {language.know}
                         </div>
+                        <LazyImage src={next} className='theory__button__navigate' onClick={() => { change("NEXT"); }} />
                         <div className="theory__fill">
 
                         </div>
